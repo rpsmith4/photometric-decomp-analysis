e64fbd083fb76d8def4499102262437951dc6de8
rpsmith4 <149445109+rpsmith4@users.noreply.github.com>
2025-07-10 11:55:53 -0600

    Can do some rudimentary plotting now

--- plot_parameters.py
+++ plot_parameters.py
@@ -14,6 +14,7 @@ from scipy.ndimage import rotate
 import scipy.integrate as integrate
 import scipy
 import pyimfit
+import pandas as pd
 
 import argparse
 from pathlib import Path
@@ -24,9 +25,12 @@ from photutils.isophote import Ellipse
 from photutils.isophote import build_ellipse_model
 
 import astropy.units as u
+import warnings
+warnings.filterwarnings("ignore")
 
-def parse_results(file):
+def parse_results(file, galaxy_name):
     model = pyimfit.parse_config_file(file)
+    band = Path(file).stem[0]
     # print(model.functionLabelList())
     # print(model.functionNameList())
     # print(model.functionList()[i].parameterList()[0].value)
@@ -54,34 +58,71 @@ def parse_results(file):
         #         print(f"{parameter.name} : {parameter.value}")
         
         func_dict = function.getFunctionAsDict()
+        for param in func_dict["parameters"]:
+            func_dict["parameters"][param] = func_dict["parameters"][param][0]
         if k == 0:
             func_dict["label"] = "Host"
         if k == 1:
             func_dict["label"] = "Polar"
+        func_dict["band"] = band
+        func_dict["Galaxy"] = galaxy_name
+        # TODO: Get other parameters here (or somewhere somehow)
+        e = func_dict["parameters"]["ell"]
+        axis_ratio = np.sqrt(-1*(np.square(e) - 1)) # b/a ratio
+        func_dict["b/a"] = axis_ratio
         functions.append(func_dict)
-        print(functions)
     
-    return None
+    return functions, chi_sq, chi_sq_red, status, status_message
 
+def plot(all_functions):
+    # TODO: Will have to account for the different types of fits at some point
+    # print(all_functions)
+    # df = pd.DataFrame(all_functions)
+    df = pd.json_normalize(all_functions)
+    df = df.groupby(by="Galaxy", group_keys=True)[df.columns].apply(lambda x: x)
+    host_PA = df[df["label"] == "Host"]["parameters.PA"]
+    plt.hist(host_PA, density=True, histtype='step', facecolor='g')
+    plt.savefig("test.png")
+    
+    #     for function in functions:
+    #         if function["label"] == "Host":
+    #             print("")
+    #             # host_PAs.append(function["PA"])
+                
+    return None
 
 def main(args):
     p = Path(args.p)
+    threshold = 1 # For reduced chi-sq
     if args.r:
         structure = os.walk(p)
         for root, dirs, files in structure:
             if not(files == []):
                 model_files = sorted(glob.glob(os.path.join(Path(root), "?_fit_params.txt")))
-                print(model_files)
-                for model in model_files:
-                    model = pyimfit.ModelDescription.load(model)
-                    print(model)
+                # print(model_files)
+                for model_file in model_files:
+                    functions, chi_sq, chi_sq_red, status, status_message = parse_results(model_file, root.base_name())
     else:
-        # model_files = sorted(glob.glob(os.path.join(p, "?_fit_params.txt")))
         model_files = sorted(glob.glob(os.path.join(p, "?_fit_params.txt")))
-        print(model_files)
+        all_functions = []
         for model_file in model_files:
-            # model = pyimfit.ModelDescription.load(model)
-            resutls = parse_results(model_file)
+            functions, chi_sq, chi_sq_red, status, status_message = parse_results(model_file, os.path.basename(p))
+            all_functions.extend(functions)
+            
+            threshold = 1
+            if chi_sq_red > threshold:
+                warnings.warn(f"{Path(model_file).resolve().relative_to(p.resolve())} has high reduced chi-sq! ({chi_sq_red} > {threshold})")
+        plot(all_functions)
+
+def _warning(
+    message,
+    category = UserWarning,
+    filename = '',
+    lineno = -1,
+    file = '',
+    line = -1):
+    print(message)
+warnings.showwarning = _warning
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Hello")
