a9abf79d4f5c324fceb9fad5dc94d6c9e82a78fa
rpsmith4 <149445109+rpsmith4@users.noreply.github.com>
2025-07-11 14:50:10 -0600

    Added more plotting stuff

--- plot_parameters.py
+++ plot_parameters.py
@@ -26,7 +26,13 @@ from photutils.isophote import build_ellipse_model
 
 import astropy.units as u
 import warnings
-warnings.filterwarnings("ignore")
+
+import sys
+IMAN_DIR = os.path.expanduser("~/Documents/iman_new")
+sys.path.append(os.path.join(IMAN_DIR, 'decomposition/make_model'))
+import make_model_ima_imfit
+
+# warnings.filterwarnings("ignore")
 
 def parse_results(file, galaxy_name):
     model = pyimfit.parse_config_file(file)
@@ -74,15 +80,94 @@ def parse_results(file, galaxy_name):
     
     return functions, chi_sq, chi_sq_red, status, status_message
 
-def plot(all_functions):
+def quantities_plot(all_functions):
     # TODO: Will have to account for the different types of fits at some point
     # print(all_functions)
     # df = pd.DataFrame(all_functions)
     df = pd.json_normalize(all_functions)
     df = df.groupby(by="Galaxy", group_keys=True)[df.columns].apply(lambda x: x)
-    host_PA = df[df["label"] == "Host"]["parameters.PA"]
-    plt.hist(host_PA, density=True, histtype='step', facecolor='g')
-    plt.savefig("test.png")
+    # TODO: Add each band (maybe overlay them on the same plots, just different colors)
+    fig = plt.figure()
+    plt.suptitle("Host")
+    band_colors = {
+        "g": "g",
+        "r" : "r",
+        "i" : "firebrick",
+        "z" : "darkred"
+    }
+    for band in "griz":
+        df_band = df[df["band"] == band].copy()
+        host_PA = df_band[df_band["label"] == "Host"]["parameters.PA"]
+        host_ax_ratio = df_band[df_band["label"] == "Host"]["b/a"]
+        host_I_e = df_band[df_band["label"] == "Host"]["parameters.I_e"]
+        host_r_e = df_band[df_band["label"] == "Host"]["parameters.r_e"]
+        host_n = df_band[df_band["label"] == "Host"]["parameters.n"]
+
+
+        plt.subplot(2, 3, 1)
+        plt.hist(host_PA, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel("PA")
+        plt.ylabel("Count")
+
+        plt.subplot(2, 3, 2)
+        plt.hist(host_ax_ratio, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel("Axis ratio (b/a)")
+        plt.ylabel("Count")
+
+        plt.subplot(2, 3, 3)
+        plt.hist(host_I_e, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel(r"Half light intensity $I_e$")
+        plt.ylabel("Count")
+
+        plt.subplot(2, 3, 4)
+        plt.hist(host_r_e, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel(r"Half light radius $r_e$")
+        plt.ylabel("Count")
+
+        ax = plt.subplot(2, 3, 5)
+        plt.hist(host_n, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel(r"Sersic Index $n$")
+        plt.ylabel("Count")
+    ax.legend(bbox_to_anchor=(1.1, 1.05))
+    plt.tight_layout()
+    plt.savefig(os.path.join(Path(args.o), "host.png"))
+
+    for band in "griz":
+        df_band = df[df["band"] == band].copy()
+        polar_PA = df_band[df_band["label"] == "polar"]["parameters.PA"]
+        polar_ax_ratio = df_band[df_band["label"] == "polar"]["b/a"]
+        polar_I_e = df_band[df_band["label"] == "polar"]["parameters.I_e"]
+        polar_r_e = df_band[df_band["label"] == "polar"]["parameters.r_e"]
+        polar_n = df_band[df_band["label"] == "polar"]["parameters.n"]
+
+
+        plt.subplot(2, 3, 1)
+        plt.hist(polar_PA, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel("PA")
+        plt.ylabel("Count")
+
+        plt.subplot(2, 3, 2)
+        plt.hist(polar_ax_ratio, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel("Axis ratio (b/a)")
+        plt.ylabel("Count")
+
+        plt.subplot(2, 3, 3)
+        plt.hist(polar_I_e, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel(r"Half light intensity $I_e$")
+        plt.ylabel("Count")
+
+        plt.subplot(2, 3, 4)
+        plt.hist(polar_r_e, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel(r"Half light radius $r_e$")
+        plt.ylabel("Count")
+
+        ax = plt.subplot(2, 3, 5)
+        plt.hist(polar_n, histtype='step', color=band_colors[band], label=band)
+        plt.xlabel(r"Sersic Index $n$")
+        plt.ylabel("Count")
+    ax.legend(bbox_to_anchor=(1.1, 1.05))
+    plt.tight_layout()
+    plt.savefig(os.path.join(Path(args.o), "polar.png"))
     
     #     for function in functions:
     #         if function["label"] == "Host":
@@ -93,7 +178,10 @@ def plot(all_functions):
 
 def main(args):
     p = Path(args.p)
-    threshold = 1 # For reduced chi-sq
+    threshold = args.t # For reduced chi-sq
+    all_functions = []
+    total_bad_fit = 0
+    total_fit = 0
     if args.r:
         structure = os.walk(p)
         for root, dirs, files in structure:
@@ -101,18 +189,42 @@ def main(args):
                 model_files = sorted(glob.glob(os.path.join(Path(root), "?_fit_params.txt")))
                 # print(model_files)
                 for model_file in model_files:
-                    functions, chi_sq, chi_sq_red, status, status_message = parse_results(model_file, root.base_name())
+                    functions, chi_sq, chi_sq_red, status, status_message = parse_results(model_file, os.path.basename(root))
+                    img_file = f"image_{functions[0]["band"]}.fits"
+                    psf_file = f"psf_patched_{functions[0]["band"]}.fits"
+                    params_file = f"{functions[0]["band"]}_fit_params.txt"
+                    os.chdir(Path(root).resolve())
+                    try:
+                        make_model_ima_imfit.main(img_file, params_file, psf_file, composed_model_file=f"{functions[0]["band"]}_composed.fits", comp_names=["Host", "Polar"])
+                    except:
+                        print(f"Failed for {Path(img_file).stem}")
+                    all_functions.extend(functions)
+                    
+                    total_fit += 1
+                    if chi_sq_red > threshold or chi_sq_red != chi_sq_red:
+                        warnings.warn(f"{Path(model_file).resolve().relative_to(p.resolve())} has high reduced chi-sq! ({chi_sq_red} > {threshold})", UserWarning)
+                        total_bad_fit += 1
     else:
         model_files = sorted(glob.glob(os.path.join(p, "?_fit_params.txt")))
-        all_functions = []
         for model_file in model_files:
             functions, chi_sq, chi_sq_red, status, status_message = parse_results(model_file, os.path.basename(p))
+            img_file = f"image_{functions[0]["band"]}.fits"
+            psf_file = f"psf_patched_{functions[0]["band"]}.fits"
+            params_file = f"{functions[0]["band"]}_fit_params.txt"
+            os.chdir(p.resolve())
+            try:
+                make_model_ima_imfit.main(img_file, params_file, psf_file, composed_model_file=f"{functions[0]["band"]}_composed.fits", comp_names=["Host", "Polar"])
+            except:
+                print(f"Failed for {Path(img_file).stem}")
             all_functions.extend(functions)
             
-            threshold = 1
+            total_fit += 1
             if chi_sq_red > threshold:
                 warnings.warn(f"{Path(model_file).resolve().relative_to(p.resolve())} has high reduced chi-sq! ({chi_sq_red} > {threshold})")
-        plot(all_functions)
+                total_bad_fit += 1
+    quantities_plot(all_functions)
+    print(f"Total fit: {total_fit}")
+    print(f"Total poor fit: {total_bad_fit} ({total_bad_fit/total_fit * 100:.2f}% bad)")
 
 def _warning(
     message,
@@ -121,14 +233,24 @@ def _warning(
     lineno = -1,
     file = '',
     line = -1):
-    print(message)
+    if UserWarning:
+        print(f"{message}")
+    else:
+        print(f"{category}: {message} at {lineno}")
 warnings.showwarning = _warning
+# class customWarning(Warning):
+#     def __init__(self, message):
+#         self.message = message
+#     def __str__(self):
+#         return repr(self.message)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Hello")
-    parser.add_argument("-p", help="Path to file/folder containing models")
+    parser.add_argument("-p", help="Path to file/folder containing models", default=".")
     parser.add_argument("--type", help="Type of polar structure", choices=["ring", "bulge", "halo"])
     parser.add_argument("-r", help="Recursively go into subfolders (assumes that fits data is at the end of the filetree)", action="store_true")
-
+    parser.add_argument("-t", help="Reduced Chi-Sq threshold for a fit to be considered bad",type=float, default=1)
+    parser.add_argument("-o", help="Output of overall statistics plot", default=".")
     args = parser.parse_args()
+    args.o = Path(args.o).resolve()
     main(args)
