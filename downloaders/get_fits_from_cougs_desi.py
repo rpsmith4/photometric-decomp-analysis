3c5c7a07a760d6a107ad590965deba187137c74b
rpsmith4 <149445109+rpsmith4@users.noreply.github.com>
2026-01-03 17:33:37 -0500

    Now using the average of all the given R26 values from both IRAF and SGA as to reduce how many galaxies don't have R26 values

--- downloaders/get_fits_from_cougs_desi.py
+++ downloaders/get_fits_from_cougs_desi.py
@@ -1,6 +1,6 @@
 from astropy.io import fits
 import numpy as np
-from astropy.table import Table
+from astropy.table import QTable
 import argparse
 import os
 from pathlib import Path
@@ -56,51 +56,52 @@ from functools import partial
 #         if not(args.no_make_folder):
 #             os.chdir("..")
 
-def get_fits(i, file_names, RA, DEC, R26, psg_types, args):
+def get_fits(file_names, RA, DEC, R26, psg_types, args):
     args.factor = float(args.factor)
     curr_root = Path(os.getcwd())
     # pixscale = 0.262
     # RR = int(np.ceil(R26[k]*60. * args.factor/pixscale))
-    file = file_names[i]
-    psg_type = psg_types[i]
+    for i in range(len(file_names)):
+        file = file_names[i]
+        psg_type = psg_types[i]
 
-    directory = os.path.join(psg_type, file)
-    print(f"Downloading {file}...")
-    if not(args.no_make_folder):
-        os.makedirs(directory, exist_ok=True)
-        os.chdir(directory)
-    if "fits" in args.files and (not any(os.path.isfile(f"image_{band}.fits") for band in args.bands) or args.overwrite):
-        download_legacy_DESI.main([file], [RA[i]], [DEC[i]], R=[R26[i]*args.factor], file_types=["fits"], bands=args.bands, dr=args.dr)
+        directory = os.path.join(psg_type, file)
+        print(f"Downloading {file}...")
+        if not(args.no_make_folder):
+            os.makedirs(directory, exist_ok=True)
+            os.chdir(directory)
+        if "fits" in args.files and (not any(os.path.isfile(f"image_{band}.fits") for band in args.bands) or args.overwrite):
+            download_legacy_DESI.main([file], [RA[i]], [DEC[i]], R=[R26[i]*args.factor], file_types=["fits"], bands=args.bands, dr=args.dr)
 
-    if "psf" in args.files and (not any(os.path.isfile(f"psf_core_{band}.fits") for band in args.bands) or args.overwrite):
-        download_legacy_DESI.main([file + "_psf"], [RA[i]], [DEC[i]], R=[R26[i]*args.factor], file_types=["psf"], bands=args.bands, dr=args.dr)
+        if "psf" in args.files and (not any(os.path.isfile(f"psf_core_{band}.fits") for band in args.bands) or args.overwrite):
+            download_legacy_DESI.main([file + "_psf"], [RA[i]], [DEC[i]], R=[R26[i]*args.factor], file_types=["psf"], bands=args.bands, dr=args.dr)
 
 
-    if "mask" in args.files and (not(os.path.isfile("image_mask.fits")) or args.overwrite):
-        pixscale = 0.262
-        RR = int(np.ceil(R26[i]*60. * args.factor/pixscale))
+        if "mask" in args.files and (not(os.path.isfile("image_mask.fits")) or args.overwrite):
+            pixscale = 0.262
+            RR = int(np.ceil(R26[i]*60. * args.factor/pixscale))
 
-        total_mask = np.zeros((RR*2, RR*2))
+            total_mask = np.zeros((RR*2, RR*2))
 
-        for k, band in enumerate(args.bands):
-            try:
-                image_dat = fits.open("image_" + band + ".fits")[0].data
+            for k, band in enumerate(args.bands):
                 try:
-                    image, mask, theta, sma, smb = get_mask.prepare_rotated(image_dat, subtract=False, rotate_ok=False)
-                    total_mask += mask
+                    image_dat = fits.open("image_" + band + ".fits")[0].data
+                    try:
+                        image, mask, theta, sma, smb = get_mask.prepare_rotated(image_dat, subtract=False, rotate_ok=False)
+                        total_mask += mask
+                    except:
+                        continue 
                 except:
-                    continue 
-            except:
-                continue
+                    continue
 
-        total_mask[total_mask >= 1] = 1
-        file_name = "image_mask.fits"
-        fits.PrimaryHDU(total_mask).writeto(file_name, overwrite=args.overwrite)
+            total_mask[total_mask >= 1] = 1
+            file_name = "image_mask.fits"
+            fits.PrimaryHDU(total_mask).writeto(file_name, overwrite=args.overwrite)
 
-    if "jpg" in args.files and (not(os.path.isfile(f"image.jpg")) or args.overwrite):
-        download_legacy_DESI.main([file], [RA[i]], [DEC[i]], R=[R26[i]*args.factor], file_types=["jpg"], bands=args.bands, dr=args.dr)
-    if not(args.no_make_folder):
-        os.chdir(curr_root)
+        if "jpg" in args.files and (not(os.path.isfile(f"image.jpg")) or args.overwrite):
+            download_legacy_DESI.main([file], [RA[i]], [DEC[i]], R=[R26[i]*args.factor], file_types=["jpg"], bands=args.bands, dr=args.dr)
+        if not(args.no_make_folder):
+            os.chdir(curr_root)
 
 
 def get_quantities(files, data):
@@ -112,18 +113,22 @@ def get_quantities(files, data):
 
 def main(args):
     # data = Table.read(args.c + "SGA-2020.fits")
-    catalog = Table.read("master_table.csv", data_start=2)
+    catalog = QTable.read("master_table.csv", data_start=2)
 
     if not(args.o == None):
         output = Path(args.o).resolve()
+        os.makedirs(output, exist_ok=True)
         os.chdir(output)
     
     # print(catalog.colnames)
     file_names = catalog["NAME"]
     RA = np.array(catalog["RA"]).astype(np.float64)
     DEC = np.array(catalog["DEC"].astype(np.float64))
-    R26 = np.array(catalog["D26_SGA"]/2).astype(np.float64)
-    R26[R26 == 0] = 0.3 # Get rid of empty values
+    R26_SGA = np.array(catalog["D26_SGA"]/2).astype(np.float64)
+    R26_IRAF = np.array([catalog["R26_G_IRAF"], catalog["R26_R_IRAF"], catalog["R26_I_IRAF"], catalog["R26_Z_IRAF"]])
+    R26 = np.append(R26_IRAF, np.expand_dims(R26_SGA, 0), axis=0)
+    R26 = np.nanmean(R26, axis=0, where=(R26 != 0))
+    # R26[R26 == 0] = 0.3 # Get rid of empty values
 
     types = catalog["PSG_TYPE_1"]
 
@@ -132,8 +137,7 @@ def main(args):
     # part = partial(get_fits, file_names=file_names, RA=RA, DEC=DEC, R26=R26, psg_types = types, args=args)
     # with MPIPoolExecutor(max_workers=1) as pool:
     #     pool.map(part, idx)
-    for i in idx:
-        get_fits(file_names=file_names, RA=RA, DEC=DEC, R26=R26, psg_types = types, args=args, i=i)
+    get_fits(file_names=file_names, RA=RA, DEC=DEC, R26=R26, psg_types = types, args=args)
 
     # get_fits(file_names, RA, DEC, R26, args)
 
