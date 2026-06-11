92a281c08ea0e16e97971f3aa3ee3c5e4eff9c93
rpsmith4 <ryan.smith.p287@gmail.com>
2026-06-11 00:54:55 -0500

    New parameter plotting script made (just parses results into a Qtable at the moment)

---
+++ fitresults/parse_results.py
@@ -0,0 +1,142 @@
+import pyimfit
+from pathlib import Path
+import astropy.units as u
+import astropy.constants as c
+import subprocess
+import numpy as np
+import os
+from astropy.table import QTable
+import traceback as tb
+import astropy
+
+
+def get_all_results(galaxies, bands, fit_type, galmarks):
+    # galnames = [os.path.basename(galaxy) for galaxy,_ in galaxies]
+    # results["Galaxy_Name"] = galnames
+
+    parameter_names = set()
+    rows = []
+
+    for galaxy_path, galaxy_type in galaxies:
+        galaxy_name =  os.path.basename(galaxy_path)
+        if galaxy_name in galmarks.keys():
+            if galmarks[galaxy_name] != "unable":
+                for band in bands:
+                    try:
+                        file = os.path.join(galaxy_path, f"{fit_type}_{band}_fit_params.txt")
+                        functions, chi_sq, chi_sq_red, status, status_message = parse_results(file)
+                        for function in functions:
+                            parameter_names.update(list(function["parameters"].keys()))
+                            rows.append(
+                                {
+                                    "Galaxy Name": galaxy_name,
+                                    "Galaxy Type": galaxy_type,
+                                    "Fit Function": function["name"],
+                                    "Function Label": function["label"],
+                                    "band": band,
+                                    "b/a": function["b/a"],
+                                    "Flux Ratio": function["flux_ratio"],
+                                    "Flux": function["flux"],
+                                    "parameters": function["parameters"],
+                                    "ChiSq": chi_sq,
+                                    "Reduced ChiSq": chi_sq_red,
+                                    "Fit Status": status.strip(),
+                                    "Fit Status Message": status_message.strip()
+                                }
+                            )
+
+                    except Exception:
+                        print(f"Couldn't read {file}")
+                        # print(tb.format_exc())
+
+    names = [name for name in rows[0].keys() if name!="parameters"]
+    column_data = {name: [] for name in names}
+    for parameter_name in sorted(parameter_names):
+        column_data[parameter_name] = []
+
+    for row in rows:
+        for name in names:
+            column_data[name].append(row[name])
+        for parameter_name in sorted(parameter_names):
+            column_data[parameter_name].append(row["parameters"].get(parameter_name, np.nan))
+
+    results = QTable(column_data)
+    results = results.group_by("Galaxy Name")
+    return results
+    
+
+
+def parse_results(file):
+    model = pyimfit.parse_config_file(file)
+    band = Path(file).stem.rsplit("_")[-3]
+    with open(file, "r") as f:
+        lines = f.readlines()
+    status = lines[5].split(" ")[7]
+    status_message = " ".join(lines[5].split(" ")[9:])
+    uncs = dict()
+    func_labels = list()
+    for k, line in enumerate(lines):
+        if "FUNCTION" in line:
+            func_type = line.split(" ")[1].rstrip()
+            func_label = line.split("LABEL ")[-1].rstrip()
+            func_labels.append(func_label)
+            func_params = pyimfit.get_function_dict()[func_type]
+            uncs[func_label] = dict()
+            for j, func_param in enumerate(func_params):
+                try:
+                    unc = lines[k + j + 1].split("+/-")[1].split("\t")[0]
+                    uncs[func_label][func_param] = float(unc) # Extremely janky way to get the uncertainties
+                except:
+                    uncs[func_label][func_param] = None
+    chi_sq = float(lines[7].split(" ")[-1])
+    chi_sq_red = float(lines[8].split(" ")[-1])
+    functions = []
+
+    flux_ratios, flux = get_flux(model_file=file, labels=func_labels)
+    for k, function in enumerate(model.functionList()):
+        func_dict = function.getFunctionAsDict()
+        for param in func_dict["parameters"]:
+            # func_dict["parameters_unc"][param] = func_dict["parameters"][param] 
+            func_dict["parameters"][param] = func_dict["parameters"][param][0]
+        func_dict["label"] = func_labels[k]
+        func_dict["parameters_unc"] = uncs[func_dict["label"]]
+        func_dict["band"] = band
+        
+        e = func_dict["parameters"]["ell"]
+        # axis_ratio = np.sqrt(-1*(np.square(e) - 1)) # b/a ratio # THIS IS WRONG I THOUGH IT WAS ECCENTRICITY BUT ITS ELLIPCTICITY
+        axis_ratio = -1*(e - 1)# f=1-b/a
+        func_dict["b/a"] = axis_ratio
+        func_dict["flux_ratio"] = flux_ratios[func_labels[k]]
+        func_dict["flux"] = flux[func_labels[k]]
+
+        functions.append(func_dict)
+    
+    return functions, chi_sq, chi_sq_red, status, status_message
+
+# TODO: Change nmgy to "flux"
+# Range should be -19 to -23 for abs mag
+def flux2ABmag(flux):
+    return 22.5 - 2.5 * np.log10(flux)
+
+def get_flux(model_file, labels):
+    result = subprocess.run(["makeimage", f"{model_file}", "--print-fluxes"], capture_output=True)
+    # print(result.stdout.decode("utf-8"))
+
+    lines = result.stdout.decode("utf-8").split("\n")
+    flux_ratios = {}
+    flux = {}
+    for line in lines:
+        line = line.split(" ")
+        line = list(filter(None, line))
+
+        for label in labels:
+            if label in line:
+                flux_ratios[label] = float(line[3])
+                flux[label] = float(line[1])
+                if float(line[3]) != float(line[3]):
+                    print(f"Nan flux ratio for {model_file}")
+    if flux_ratios == {}:
+        print(f"Unable to determine flux ratios for {model_file}")
+        for label in labels:
+            flux_ratios[label] = -1
+    return flux_ratios, flux
---
+++ fitresults/plot_parameters_new.py
@@ -0,0 +1,100 @@
+import glob
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import numpy as np
+import os
+
+from astropy.io import fits
+from astropy.constants import c
+from astropy.table import QTable
+
+import pyimfit
+import pandas as pd
+
+import argparse
+from pathlib import Path
+
+import astropy.units as u
+import subprocess
+import traceback as tb
+import sys
+import json
+
+IMAN_DIR = os.path.join(Path(os.path.dirname(__file__)).parent, "iman_new")
+sys.path.append(os.path.join(IMAN_DIR, 'decomposition/make_model'))
+import make_model_ima_imfit
+
+from parse_results import *
+
+# warnings.filterwarnings("ignore")
+
+def quantities_plot(all_functions):
+    fig = plt.figure()
+    band_colors = {
+        "g": "g",
+        "r" : "r",
+        "i" : "firebrick",
+        "z" : "blueviolet"
+    }
+    return None
+
+def main(args):
+    global p
+    p = Path(args.p).resolve()
+    SGAtable = QTable.read(os.path.join(args.c, "SGA-2020.fits"))
+    # master_table = QTable.read(os.path.join())
+    master_table = QTable.read("/home/ryans/Projects/Photometric Decomp/Analysis/master_table.csv")
+    galmarks = json.load(open(os.path.join(p, "galmarks.json")))
+    print(galmarks)
+
+    global total_bad_fit
+    global total_fit
+    global bound_sticking
+    total_bad_fit = 0
+    total_fit = 0
+    bound_sticking = 0
+
+    galaxies = []
+    bands = ["g", "r"]
+
+    structure = os.walk(p)
+    for root, dirs, files in structure:
+        if len(glob.glob(os.path.join(root,"*.fits"))) != 0: # Check to see if we have entered a folder with galaxy data
+            galaxies.append([Path(root).resolve(), os.path.basename(os.path.dirname(root))])
+
+    all_results =  get_all_results(galaxies, bands, args.fit_type, galmarks)
+    total_fit = len(all_results.groups.indices)
+    total_bad_fit = len(all_results[all_results["Reduced ChiSq"] > args.t].groups.indices)
+
+    print(f"Total fit: {total_fit}")
+    print(f"Total poor fit: {total_bad_fit} ({total_bad_fit/total_fit * 100:.2f}% bad)")
+    print(f"Total parameter bounds sticking: {bound_sticking}")
+
+    print(all_results)
+
+    if args.plot_stats:
+        quantities_plot(all_results)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Generate plots of the outputs of fits")
+    parser.add_argument("-p", help="Path to file/folder containing models", default=".")
+    parser.add_argument("--type", help="Type of polar structure", choices=["ring", "bulge", "halo"])
+    parser.add_argument("-r", help="Recursively go into subfolders (assumes that fits data is at the end of the filetree)", action="store_true")
+    parser.add_argument("-t", help="Reduced Chi-Sq threshold for a fit to be considered bad",type=float, default=1)
+    parser.add_argument("-o", help="Output of overall statistics plot", default=".")
+    parser.add_argument("--plot_stats", help="Plot overall statistics", action="store_true")
+    parser.add_argument("--make_composed", help="Make a composed image of the galaxy (includes image, model, and components)", action="store_true")
+    parser.add_argument("--overwrite", help="Overwrite existing files", action="store_true")
+    parser.add_argument("-c", help="Directory to Sienna Galaxy Atlas File (used to get redshift)", default=".")
+    parser.add_argument("--fit_type", help="Type of fit done", choices=["2_sersic", "1_sersic_1_gauss_ring", "3_sersic"], default="2_sersic")
+    parser.add_argument("--mask", help="Use mask on the original image", action="store_true")
+    parser.add_argument("--plot_type", help="Type of plots to make", choices=["compare_structure", "compare_type"], default="compare_structure")
+    parser.add_argument("--dont_exclude", help="Don't exclude galaxies in the 'exclude.txt' file", action="store_true")
+    parser.add_argument("-v", help="Show chi-sq warnings for fits", action="store_true")
+    parser.add_argument("-vv", help="Show chi-sq and parameter bounds warnings for fits", action="store_true")
+    parser.add_argument("-vvv", help="Show chi-sq and parameter bounds (specific) warnings for fits", action="store_true")
+    args = parser.parse_args()
+    args.o = Path(args.o).resolve()
+    main(args)
